ce5206d4d2118ecf1cfbb84078e858657621604b
Ole <ole@vprsm.de>
2017-10-22 23:15:05 +0100

    Sun 22 Oct 23:15:05 BST 2017

---
+++ Haskell.py
@@ -0,0 +1,13 @@
+import sublime
+import sublime_plugin
+
+import subprocess
+import os.path
+
+class StylishHaskell(sublime_plugin.EventListener):
+	def on_post_save_async(self, view):
+		file = view.file_name()
+		if view.is_scratch() or file[-3:] != '.hs':
+			return
+
+		subprocess.call(['stylish-haskell', '-i', file], cwd = os.path.dirname(file))
