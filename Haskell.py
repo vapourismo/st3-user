3e29f7a1c0ba844abf5dc6b441a3c50101ff1238
Ole <ole@vprsm.de>
2018-02-19 20:38:06 +0000

    Mon 19 Feb 20:38:06 GMT 2018

--- Haskell.py
+++ Haskell.py
@@ -9,4 +9,8 @@ class StylishHaskell(sublime_plugin.EventListener):
 		if view.is_scratch() or file[-3:] != '.hs':
 			return
 
-		subprocess.call(['stylish-haskell', '-i', file], cwd = os.path.dirname(file))
+		subprocess.call(
+			['stylish-haskell', '-i', file],
+			cwd = os.path.dirname(file),
+			shell = False
+		)
